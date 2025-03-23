230e1b61ad2f82629ea73e2ad49380d0fee234fe
Evan Luo <headlessbear06@gmail.com>
2025-03-23 17:29:45 -0600

    changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      Evan's Github pages
+      Evans Github pages
       <div>
         Projects
       </div>
