238be844eddcf22e7ae256957cdd69623eea2679
Evan Luo <headlessbear06@gmail.com>
2025-03-23 17:42:30 -0600

    added config changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: '/Evan-Luo-jpg.github.io',
+  assetPrefix: '/Evan-Luo-jpg.github.io',
 };
 
 export default nextConfig;
