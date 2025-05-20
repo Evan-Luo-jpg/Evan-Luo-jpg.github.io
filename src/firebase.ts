8db28194fc50e24dcf142a2c43cce38ef9b30b22
Evan Luo <headlessbear06@gmail.com>
2025-05-19 23:21:11 -0600

    changed

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,7 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCYJY83UE2X_kyWt7qIpDHVxtux1yLcMGE",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "personalwebsite-a669e.firebaseapp.com",
   projectId: "personalwebsite-a669e",
   storageBucket: "personalwebsite-a669e.firebasestorage.app",
